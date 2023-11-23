48914190b34a11b3ffa1a9dcb0f80925c665f4d6
Angga <angga060803@gmail.com>
2023-11-23 20:39:03 +0700

    commit-2

--- 3D_Inisial/main.js
+++ 3D_Inisial/main.js
@@ -1,89 +1,98 @@
-function main(){
+function main() {
     var canvas = document.getElementById("myCanvas");
-    var gl = canvas.getContext("webgl");
+    var gl = canvas.getContext("webgl", { antialias: true });
 
-    //vertex buffer
+    function resizeCanvas() {
+        canvas.width = window.innerWidth * window.devicePixelRatio;
+        canvas.height = window.innerHeight * window.devicePixelRatio;
+        canvas.style.width = window.innerWidth + 'px';
+        canvas.style.height = window.innerHeight + 'px';
+
+        gl.viewport(0, 0, canvas.width, canvas.height);
+    }
+
+    window.addEventListener('resize', resizeCanvas);
+
+    resizeCanvas();
+
+    // Vertex buffer
     var vertexBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
-    
-    //color buffer
+
+    // Color buffer
     var colorBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW);
 
-    //index buffer
+    // Index buffer
     var indexBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
     gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indices), gl.STATIC_DRAW);
 
-    //mengambil dan menyimpan informasi vertex dari html dg document getElementById
+    // Vertex shader
     var vertexShaderCode = document.getElementById("vertexShaderCode").text;
-    //membuat vertex shader
-    var vertexShader = gl.createShader( gl.VERTEX_SHADER );
+    var vertexShader = gl.createShader(gl.VERTEX_SHADER);
     gl.shaderSource(vertexShader, vertexShaderCode);
     gl.compileShader(vertexShader);
 
-    //mengambil dan menyimpan informasi fragment dari html dg document getElementByID
+    // Fragment shader
     var fragmentShaderCode = document.getElementById("fragmentShaderCode").text;
-    //membuat fragment shader
-    var fragmentShader = gl.createShader( gl.FRAGMENT_SHADER );
+    var fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
     gl.shaderSource(fragmentShader, fragmentShaderCode);
     gl.compileShader(fragmentShader);
 
-    //menambahkan info shader ke package agar bisa dicompile
-    var program = gl.createProgram();  
-    gl.attachShader(program, vertexShader);   
-    gl.attachShader(program, fragmentShader);   
+    // Shader program
+    var program = gl.createProgram();
+    gl.attachShader(program, vertexShader);
+    gl.attachShader(program, fragmentShader);
     gl.linkProgram(program);
     gl.useProgram(program);
 
-    //menambahkan vertices ke dalam aPosition dan aColor untuk digambar
-    //position
+    // Position attribute
     gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
     var aPosition = gl.getAttribLocation(program, "aPosition");
     gl.vertexAttribPointer(aPosition, 3, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(aPosition);
 
-    //color
+    // Color attribute
     gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
     var aColor = gl.getAttribLocation(program, "aColor");
     gl.vertexAttribPointer(aColor, 3, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(aColor);
-    
+
+    // Uniform locations
     var Pmatrix = gl.getUniformLocation(program, "uProj");
     var Vmatrix = gl.getUniformLocation(program, "uView");
     var Mmatrix = gl.getUniformLocation(program, "uModel");
-    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
-    
+
+    // Projection matrix
     var projmatrix = glMatrix.mat4.create();
-    var modmatrix = glMatrix.mat4.create();
+    glMatrix.mat4.perspective(projmatrix, glMatrix.glMatrix.toRadian(60), canvas.width / canvas.height, 0.1, 10.0);
+
+    // View matrix
     var viewmatrix = glMatrix.mat4.create();
+    glMatrix.mat4.lookAt(viewmatrix, [0.0, 0.0, 3.0], [0.0, 0.0, 0.0], [0.0, 1.0, 0.0]);
 
-    glMatrix.mat4.perspective(projmatrix, //matriks proyeksi
-        glMatrix.glMatrix.toRadian(90), //sudutnya
-        1.0, //aspect ratio
-        0.5, //near
-        10.0 //far
-        );
+    // Model matrix
+    var modmatrix = glMatrix.mat4.create();
 
-    glMatrix.mat4.lookAt(viewmatrix,//matriks view
-        [0.0, 0.0, 2.0],//posisi kamera (posisi)
-        [0.0, 0.0, -2.0],//arah kamera menghadap (vektor)
-        [0.0, 1.0, 0.0]//arah atas kamera (vektor)
-        );
+    var theta = glMatrix.glMatrix.toRadian(1);
 
-    var theta = glMatrix.glMatrix.toRadian(1);//sudutnya adalah 1 derajat   
-    var animate = function(){
-        if(!freeze){
-           glMatrix.mat4.rotate(modmatrix, modmatrix, theta, [0.1,0.0,0.0]);
+    var animate = function () {
+        if (!freeze) {
+            glMatrix.mat4.rotate(modmatrix, modmatrix, theta, [0.1, 0.0, 0.0]);
         }
-        
+
         gl.enable(gl.DEPTH_TEST);
         gl.depthFunc(gl.LEQUAL);
+        gl.canvas.width = window.innerWidth * window.devicePixelRatio;
+        gl.canvas.height = window.innerHeight * window.devicePixelRatio;
 
-        gl.clearColor(1.0, 1.0, 1.0, 1.0);
+        gl.clearColor(0.2, 0.2, 0.2, 1.0); // Background color
         gl.clearDepth(1.0);
+        gl.enable(gl.BLEND);
+        gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
 
         gl.viewport(0.0, 0.0, canvas.width, canvas.height);
         gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
@@ -96,6 +105,7 @@ function main(){
         gl.drawElements(gl.TRIANGLES, indices.length, gl.UNSIGNED_SHORT, 0);
 
         window.requestAnimationFrame(animate);
-    }    
-    animate(0);    
+    };
+
+    animate(0);
 }
